52c51b467d32de9738d5c6f6efb9faf45a1d6d6f
VeljkoPerisic <veljkoprsc996@gmail.com>
2021-04-06 15:32:15 +0200

    Sending link reqests test

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,33 +1,14 @@
-
-import io.appium.java_client.MobileBy;
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.interactions.Pause;
-import org.openqa.selenium.interactions.PointerInput;
-import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import static java.util.Collections.singletonList;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-
-import static java.time.Duration.ofMillis;
-
-
-
-
-public class AndroidAppium {
-
+public class RegistrationTest extends AndroidAppium {
 
     AndroidDriver driver;
 
@@ -50,12 +31,10 @@ public class AndroidAppium {
 
         System.out.println("Pokrenuto");
 
-
     }
 
     @Test
-    public void Login() throws InterruptedException {
-
+    public void sendingLinkRequest() throws InterruptedException {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.findElement(By.id("linguado.com.linguado:id/btnGoLinguado")).click();
@@ -69,27 +48,22 @@ public class AndroidAppium {
         System.out.println("Korisnik ulogovan");
         driver.findElement(By.id("linguado.com.linguado:id/btnNext")).click();
         driver.findElement(By.id("linguado.com.linguado:id/ivFlag")).click();
+        System.out.println("Visibility tutorijal dismisovan");
         driver.findElement(By.id("linguado.com.linguado:id/scVisibilitySwitch")).click();
         Thread.sleep(2000);
         driver.findElement(By.id("linguado.com.linguado:id/scVisibilitySwitch")).click();
+        System.out.println("Filter tutorijal dismisovan");
         Thread.sleep(3000);
-        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout[4]/android.widget.ImageView[@bounds='[1182,2656][1266,2740]']")).click();
-        System.out.println("Podesavanja otvorena");
-        driver.findElement(By.id("ivSettings")).click();
-        Thread.sleep(1000);
-        System.out.println("Tutorijal za link requestove dismissovan");
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget." +
+                "FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget." +
+                "FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.ImageView")).click();
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget." +
+                "LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/androidx.viewpager.widget.ViewPager/android.widget." +
+                "RelativeLayout/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget." +
+                "RelativeLayout[1]/android.widget.ImageView[3][@bounds='[1188,525][1356,693]']")).click();
+        System.out.println("Link request tutorijal dismisovan");
 
-        // Vertical scroll
-        MobileElement element = (MobileElement) driver.findElement(MobileBy.AndroidUIAutomator(
-                "new UiScrollable(new UiSelector().scrollable(true))" +
-                        ".scrollIntoView(new UiSelector().resourceIdMatches(\".*linguado.com.linguado:id/tvLogout.*\"))"));
-        System.out.println("Skrolovano");
-
-        driver.findElement(By.id("linguado.com.linguado:id/tvLogout")).click();
-        System.out.println("Korisnik izlogovan");
     }
+
+
 }
-
-
-
-
